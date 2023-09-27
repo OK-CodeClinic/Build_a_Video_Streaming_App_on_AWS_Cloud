65324a9a84f201a7189a2b56dff655938d50b66d
Kehinde Omokungbe Kardozo <kehindefranklin@hmail.com>
2023-09-27 15:04:14 +0100

    Updating code

--- my-video-streaming-app/src/App.js
+++ my-video-streaming-app/src/App.js
@@ -6,7 +6,6 @@ function App() {
       <h2>Welcome to my Video Streaming App</h2>
       <video width="700px" height="400px" controls>
         <source src="https://d2xpign8lqkkjx.cloudfront.net/SuperbVideo.mp4" type="video/mp4"/>
-
       </video>
 
     </div>
