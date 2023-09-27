c9220989223b5627677854fba9a80d1f92f818fa
Kehinde Omokungbe Kardozo <kehindefranklin@hmail.com>
2023-09-27 14:12:47 +0100

    Uploading video artifact

--- my-video-streaming-app/src/App.js
+++ my-video-streaming-app/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Welcome to my Video Streaming App</h2>
+      <video width="700px" height="400px" controls>
+        <source src="https://d2xpign8lqkkjx.cloudfront.net/SuperbVideo.mp4" type="video/mp4"/>
+
+      </video>
+
     </div>
   );
 }
